2f02fa053cdfd03a467801a163675e7e026e6e40
matheswaaran <matheswaarans@gmail.com>
2020-12-11 00:42:18 +0530

    Added: TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    // mode: "all",
+    content: ["./src/pages/**/*.js", "./src/components/**/*.js"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
