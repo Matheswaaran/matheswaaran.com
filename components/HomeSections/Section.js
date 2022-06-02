7c7879b34671c23070b276029de3cf1d23b386c9
matheswaaran <matheswaarans@gmail.com>
2022-06-02 15:23:46 +0530

    fix: Add background rectangle to section headers

--- components/HomeSections/Section.js
+++ components/HomeSections/Section.js
@@ -6,7 +6,10 @@ const Section = (props) => {
     <div
       className="pl-8 pt-8 border-8 border-secondary/20 dark:border-primary/20 absolute top-[-75px] left-[-150px] w-[50vw] h-[50vh]"
     >
-      <span className="font-gilroy-semibold text-6xl text-highlight dark:text-white">{props.title}</span>
+      <span className="font-gilroy-semibold text-6xl text-highlight dark:text-white relative">
+        {props.title}
+        <div className="absolute left-0 top-12 w-full h-[44px] bg-primary opacity-20"/>
+      </span>
     </div>
   );
 }
