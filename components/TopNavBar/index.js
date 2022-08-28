99e44da14be97cb64d7139bb1bd67d668fcbffd2
matheswaaran <matheswaarans@gmail.com>
2022-08-28 11:49:56 +0530

    fix: Add header and Menu changes

--- components/TopNavBar/index.js
+++ components/TopNavBar/index.js
@@ -4,35 +4,48 @@ import SiteData from "../../data/SiteData.json";
 import Button from "../Button";
 import ThemeToggle from "./ThemeToggle";
 import SocialMediaProfiles from "./SocialMediaProfiles";
+import Menus from "./Menus";
 
-import {Menu} from "styled-icons/heroicons-outline";
+import { Menu } from "styled-icons/heroicons-outline";
+import { Close } from "@styled-icons/material";
 
 const TopNavBar = (props) => {
+  const [openMenu, setOpenMenu] = React.useState(false);
   return (
-    <div className="flex items-center justify-between border-grey dark:border-grey border-b px-4">
-      <span className="text-base font-gilroy-semibolditalic">
-        {SiteData.title}
-      </span>
-      <div className="flex items-center justify-end">
-        <div className="pr-4 py-2">
-          <Button to={SiteData.resume} external={true}>
-            Resume
-          </Button>
-        </div>
-        <div className="border-grey dark:border-grey border-r border-l p-4">
-          <ThemeToggle/>
-        </div>
-        <div className="border-grey dark:border-grey border-r p-4 space-x-2">
-          <SocialMediaProfiles/>
-        </div>
-        <div className="flex items-center justify-end border-grey dark:border-grey p-4 pr-0">
-          <span className="text-base font-gilroy-medium text-highlight dark:text-white mr-4">
-            Menu
-          </span>
-          <Menu className="cursor-pointer text-secondary dark:text-primary h-5 w-5"/>
+    <React.Fragment>
+      <div className="flex items-center justify-between border-grey dark:border-grey border-b px-4">
+        <span className="text-base font-gilroy-semibolditalic">
+          {SiteData.title}
+        </span>
+        <div className="flex items-center justify-end">
+          <div className="pr-4 py-2 hidden lg:block">
+            <Button to={SiteData.resume} external={true}>
+              Resume
+            </Button>
+          </div>
+          <div className="border-grey dark:border-grey border-r border-l p-4">
+            <ThemeToggle />
+          </div>
+          <div className="border-grey dark:border-grey border-r p-4 space-x-2 hidden lg:block">
+            <SocialMediaProfiles />
+          </div>
+          <div
+            className="flex items-center justify-end border-grey dark:border-grey p-4 pr-0 cursor-pointer"
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <span className="text-base font-gilroy-medium text-highlight dark:text-white mr-4">
+              Menu
+            </span>
+            {openMenu ? (
+              <Close className="text-secondary dark:text-primary h-5 w-5" />
+            ) : (
+              <Menu className="text-secondary dark:text-primary h-5 w-5" />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      {openMenu && <Menus open={openMenu} setOpen={setOpenMenu} />}
+    </React.Fragment>
   );
 };
 
