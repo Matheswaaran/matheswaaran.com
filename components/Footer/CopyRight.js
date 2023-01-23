3039ae4d76b23da76ad37ba6a18c050e8862a93a
matheswaarans <mat@edisoninteractive.com>
2023-01-23 20:56:21 +0530

    refactor: Prettier fixes

--- components/Footer/CopyRight.js
+++ components/Footer/CopyRight.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Heart} from "styled-icons/boxicons-solid";
+import { Heart } from "styled-icons/boxicons-solid";
 
 const CopyRight = (props) => {
   return (
@@ -14,7 +14,7 @@ const CopyRight = (props) => {
       </div>
       <div>
         <span className="mr-1">Made with</span>
-        <Heart className="text-secondary dark:text-primary h-5 w-5 mx-1"/>
+        <Heart className="text-secondary dark:text-primary h-5 w-5 mx-1" />
         <span className="mx-1">by</span>
         <a
           className="text-secondary dark:text-primary mx-1"
